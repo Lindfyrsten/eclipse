45273e7fcb83e532dd66fcbd81cbb768f4d71c6d
Kristian <kristianlindbjerg@live.dk>
2016-09-28 17:17:53 +0200

    Gris fixed.

--- Copy-O/src/CopierAppTest.java
+++ Copy-O/src/CopierAppTest.java
@@ -1,36 +1,35 @@
 public class CopierAppTest {
-    
-    public static void main(String[] args) throws InterruptedException {
-        
-        CopierTest c1 = new CopierTest(0);
 
-        c1.getPaper();
+    public static void main(String[] args) throws InterruptedException {
+
+        CopierTest c1 = new CopierTest(0);
+        
         c1.greeting();
-        
+
         Thread.sleep(3000);
-        
+
         while (c1.isOn == true) {
-            
+
             c1.menu();
-            
+
             if (c1.getResult() == 1) {
                 c1.makeCopy();
             }
-            
+
             else if (c1.getResult() == 2) {
                 c1.insertPaper();
             }
-            
+
             else if (c1.getResult() == 3) {
-                
+
                 c1.quit();
             }
-            
+
             else {
                 System.out.println("Please press a valid number.");
                 c1.menuRepeat();
             }
-            
+
         }
     }
 }
--- Projekt_Die/src/dicegames/PigPlay.java
+++ Projekt_Die/src/dicegames/PigPlay.java
@@ -7,12 +7,12 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class PigPlay {
-
+    
     /**
      * The scanner used for reading user input.
      */
     private Scanner scan;
-
+    
     /**
      * Name of the players
      */
@@ -22,37 +22,37 @@ public class PigPlay {
      * The die used in the game.
      */
     private Die die;
-
-    private int winNr;
     
+    private int winNr;
+
     /**
      * Points of the players
      */
     private int playerOnePoint;
     private int playerTwoPoint;
-    
-    boolean finished = false;
-    
+
+    private boolean finished = false;
+
     /**
      * Boolean to keep track of whose turn it is
      */
     private boolean pOneTurn = true;
-    
+
     /**
      * Check who won
      */
-    private boolean pOneWon;
-
+    private boolean pOneWon = true;
+    
     /**
      * Constructs the PlayRollDie game.
      */
     public PigPlay() {
-
+        
         die = new Die();
         scan = new Scanner(System.in);
         pOneTurn = true;
     }
-    
+
     /**
      * Javafx play sound function
      */
@@ -61,13 +61,14 @@ public class PigPlay {
         String uriString = new File(fileName).toURI().toString();
         new MediaPlayer(new Media(uriString)).play();
     }
-    
+
     /**
      * Print out a neat welcome message to the player.
      * @throws InterruptedException
      */
     private void welcomeToGame() throws InterruptedException {
-        System.out.println("Velkommen til PIG");
+        System.out.println("Velkommen til spillet gris!");
+        Thread.sleep(1000);
         System.out.println("Hvad er navn på første spiller?");
         playerOne = scan.nextLine();
         System.out.println("Hvad er navn på anden spiller?");
@@ -77,55 +78,59 @@ public class PigPlay {
         scan.nextLine();
         System.out.println("Lad spillet begynde!");
         Thread.sleep(2000);
-
+        
     }
-
+    
     /**
      * Finishes the game and prints out the result.
      */
     private void gameOver() {
-
+        
+        play("Applause.mp3");
         if (pOneWon) {
             System.out.println(playerOne + " vandt!");
             System.out.println("Tak for spillet");
         }
-        
+
         else {
             System.out.println(playerTwo + " vandt!");
             System.out.println("Tak for spillet");
         }
-        
+
         scan.close();
     }
-
+    
     /**
      * Player one takes a turn.
      * @throws InterruptedException
      */
     private void playerOneTurn() throws InterruptedException {
-        
+
         boolean fortsæt = true;
         int pointLost = playerOnePoint;
         while (fortsæt) {
-            System.out.println("Rolling dice...");
-            play("Roll.mp3");
-            Thread.sleep(1500);
-            
+
             die.roll();
             int roll = die.getFaceValue();
-            
+
             if (playerOnePoint >= winNr) {
-                
+
                 fortsæt = false;
                 finished = true;
-
+                
             }
             else if (die.getFaceValue() == 1) {
+                System.out.println("Rolling dice...");
+                play("Roll.mp3");
+                Thread.sleep(1500);
                 System.out.println("Øv! Du rullede 1. Ingen point denne runde");
                 playerOnePoint = pointLost;
                 fortsæt = false;
             }
             else {
+                System.out.println("Rolling dice...");
+                play("Roll.mp3");
+                Thread.sleep(1500);
                 System.out.println("");
                 System.out.println("");
                 System.out.println("Du har rullet: " + roll);
@@ -133,20 +138,20 @@ public class PigPlay {
                 System.out.println("*******************************");
                 System.out.println("Point: " + playerOnePoint);
                 System.out.println("*******************************");
-                
+
                 if (playerOnePoint < winNr) {
                     System.out.println("Vil du fortsætte: Y/N");
-                    
+
                     String goOn = scan.nextLine();
                     if (goOn.equalsIgnoreCase("N")) {
                         fortsæt = false;
                     }
-                    
+
                 }
             }
         }
         if (!fortsæt && finished) {
-
+            
             System.out.println("Vi har en vinder!!!");
             Thread.sleep(250);
             System.out.println(".");
@@ -158,16 +163,16 @@ public class PigPlay {
             System.out.println("....");
         }
         else {
-            
+
             pOneTurn = false;
             System.out.println("**************************");
             System.out.println("Skifter spiller");
             System.out.println("");
-            
+
         }
         Thread.sleep(2000);
     }
-
+    
     /**
      * Player two takes a turn.
      */
@@ -175,24 +180,27 @@ public class PigPlay {
         boolean fortsæt = true;
         int pointLost = playerTwoPoint;
         while (fortsæt) {
-            System.out.println("Rolling dice...");
-            play("Roll.mp3");
-            Thread.sleep(1500);
             die.roll();
             int roll = die.getFaceValue();
-
+            
             if (playerTwoPoint >= winNr) {
                 pOneWon = false;
                 fortsæt = false;
                 finished = true;
-                
+
             }
             else if (die.getFaceValue() == 1) {
+                System.out.println("Rolling dice...");
+                play("Roll.mp3");
+                Thread.sleep(1500);
                 System.out.println("Øv! Du rullede 1. Ingen point denne runde");
                 playerTwoPoint = pointLost;
                 fortsæt = false;
             }
             else {
+                System.out.println("Rolling dice...");
+                play("Roll.mp3");
+                Thread.sleep(1500);
                 System.out.println("");
                 System.out.println("");
                 System.out.println("Du har rullet: " + roll);
@@ -200,21 +208,21 @@ public class PigPlay {
                 System.out.println("*******************************");
                 System.out.println("Point: " + playerTwoPoint);
                 System.out.println("*******************************");
-
+                
                 if (playerTwoPoint < winNr) {
                     System.out.println("Vil du fortsætte: Y/N");
-                    
+
                     String goOn = scan.nextLine();
                     if (goOn.equalsIgnoreCase("N")) {
                         fortsæt = false;
                     }
                 }
-
+                
             }
         }
-
+        
         if (!fortsæt && finished) {
-
+            
             System.out.println("Vi har en vinder!!!");
             Thread.sleep(250);
             System.out.println(".");
@@ -226,16 +234,16 @@ public class PigPlay {
             System.out.println("....");
         }
         else {
-            
-            pOneTurn = false;
+
+            pOneTurn = true;
             System.out.println("**************************");
             System.out.println("Skifter spiller");
             System.out.println("");
-            
+
         }
         Thread.sleep(2000);
     }
-
+    
     /**
      * Start the game loop.<br/>
      * The game is finished when the player chooses to not roll the die anymore.
@@ -243,29 +251,29 @@ public class PigPlay {
      */
     public void startGame() throws InterruptedException {
         welcomeToGame();
-
+        
         while (!finished) {
-
+            
             if (pOneTurn) {
-
-                System.out.println("**************************");
-                System.out.println("");
-                System.out.println(playerOne + "'s tur.");
-                System.out.println("Du har " + playerOnePoint + " point.");
-                System.out.println("");
-                System.out.println("**************************");
+                
+                System.out.println("____________________________");
+                System.out.println("|");
+                System.out.println("| " + playerOne + "'s tur.");
+                System.out.println("| Du har " + playerOnePoint + " point.");
+                System.out.println("|");
+                System.out.println("____________________________");
                 playerOneTurn();
             }
             else {
-                System.out.println("**************************");
-                System.out.println("");
-                System.out.println(playerTwo + "'s tur.");
-                System.out.println("Du har " + playerTwoPoint + " point.");
-                System.out.println("");
-                System.out.println("**************************");
+                System.out.println("____________________________");
+                System.out.println("|");
+                System.out.println("| " + playerTwo + "'s tur.");
+                System.out.println("| Du har " + playerTwoPoint + " point.");
+                System.out.println("|");
+                System.out.println("____________________________");
                 playerTwoTurn();
             }
-            
+
         }
         gameOver();
     }
