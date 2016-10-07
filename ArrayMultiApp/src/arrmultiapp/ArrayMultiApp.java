43eed5e70b4477121b5b959e4fa1cb2ce9394625
Turfy <kristianlindbjerg@live.dk>
2016-10-07 11:57:22 +0200

    MultiArray

---
+++ ArrayMultiApp/src/arrmultiapp/ArrayMultiApp.java
@@ -0,0 +1,38 @@
+package arrmultiapp;
+
+public class ArrayMultiApp {
+    private static int row = 5;
+    private static int col = 10;
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        int[][] values = new int[row][col];
+        
+//        for (int row = 0; row < values.length; row++) {
+//            for (int col = 0; col < values[row].length; col++) {
+//                values[row][col] = 5;
+//            }
+//
+//        }
+        boolean isNul = true;
+        
+        if (isNul) {
+            
+        }
+        for (int row = 0; row < values.length; row++) {
+            for (int col = 0; col < values[row].length; col++) {
+                
+            }
+            
+        }
+        System.out.println();
+        for (int row = 0; row < values.length; row++) {
+            for (int col = 0; col < values[row].length; col++) {
+                System.out.print(values[row][col] + "  ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
--- Arrays/src/model/ArrayListApp.java
+++ Arrays/src/model/ArrayListApp.java
@@ -3,37 +3,37 @@ package model;
 import java.util.ArrayList;
 
 public class ArrayListApp {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        
         ArrayList<String> list = new ArrayList<>();
         list.add("Hans");
         list.add("Viggo");
         list.add("Jens");
         list.add("Børge");
         list.add("Bent");
-
+        
         int size = list.size();
         System.out.println("Listen indeholder " + size + " elementer."); //Udskriver antal elementer i listen
-
+        
         list.add(2, "Jane"); // Tilføjer tekststrengen ”Jane” på indeks plads 2 i listen
-
+        
         list.remove(1); //Sletter elementet på indeks plads 1 i listen
         list.add(1, "Pia");
         list.add("Ib");
         System.out.println("Listen indeholder nu " + list.size() + " elementer.");
-
+        
         list.set(2, "Hansi");
         System.out.println("Listen indeholder nu " + list.size() + " elementer.");
         System.out.println("Listen: " + list);
         System.out.println("Længden på elementerne er: ");
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) { //for each loop der udskriver string længde
             System.out.print(list.get(i).length() + " | ");
         }
         System.out.println("\n_______________________________");
-        for (String i : list) {
+        for (String i : list) { //for each loop der udskriver string længde
             System.out.print(i.length() + " | ");
         }
     }
-    
+
 }
