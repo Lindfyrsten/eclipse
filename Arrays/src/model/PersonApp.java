bc54dae6d6e026584e0fb5abf7a283223e805920
Kristian <kristianlindbjerg@live.dk>
2016-10-07 02:05:44 +0200

    AB

--- Arrays/src/model/PersonApp.java
+++ Arrays/src/model/PersonApp.java
@@ -1,13 +1,13 @@
 package model;
 
 public class PersonApp {
-    
+
     public static void main(String[] args) {
-        Person p = new Person("Hanne", 120000);
+        //Person p = new Person("Hanne", 120000);
 //		p.addJob("Håndboldtæner");
 //		p.addJob("Undeviser");
 //		p.udskrivJobs();
-    
+
     }
-    
+
 }
