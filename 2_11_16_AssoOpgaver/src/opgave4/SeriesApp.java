39078eaf69a95f12a33bfc35e3af8dd8ee117855
Kristian <kristianlindbjerg@live.dk>
2016-11-06 21:09:38 +0100

    WE

--- 2_11_16_AssoOpgaver/src/opgave4/SeriesApp.java
+++ 2_11_16_AssoOpgaver/src/opgave4/SeriesApp.java
@@ -3,19 +3,19 @@ package opgave4;
 import java.util.ArrayList;
 
 public class SeriesApp {
-
+    
     public static void main(String[] args) {
         ArrayList<String> cast = new ArrayList<>();
         ArrayList<String> guestEp1 = new ArrayList<>();
         ArrayList<String> guestEp2 = new ArrayList<>();
         String[] castStr = { "Justin Beaver", "Michael Jackson", "Donald Trump" };
         String[] guestStrEp1 = { "Kristian Lindbjerg" };
-        String[] guestStrEp2 = { "Hillary Clinton" };
-        
+        String[] guestStrEp2 = { "Hillary Clinton", "Kristian Lindbjerg" };
+
         for (String s : castStr) {
             cast.add(s);
         }
-        
+
         for (String s : guestStrEp1) {
             guestEp1.add(s);
         }
@@ -27,9 +27,9 @@ public class SeriesApp {
         Episode ep2 = new Episode(2, 55, guestEp2);
         zombz.addEpisode(ep1);
         zombz.addEpisode(ep2);
-
+        
         System.out.println("Actors: " + zombz.getGuestActors());
         System.out.println("Total length: " + zombz.totalLength() + " minutes");
-
+        
     }
 }
