8908d3eb79a0a8dfd5574f4d2d53e9743a7c5f2d
Turfy <kristianlindbjerg@live.dk>
2016-11-21 02:07:17 +0100

    o

--- 18_11_Nedarvning/src/question/QuestionDemo5.java
+++ 18_11_Nedarvning/src/question/QuestionDemo5.java
@@ -19,57 +19,54 @@ public class QuestionDemo5 extends Application {
     private TextField txfAnswer;
     private Question q = new AnyCorrectChoiceQuestion();
     private Label lblAnswer = new Label("");
-
+    
     public static void main(String[] args) {
         Application.launch(args);
-
+        
     }
-    
+
     @Override
     public void start(Stage stage) {
-
+        
         q.setText("What is the color of the famous");
         q.setAnswer("Black");
         q.setAnswer("Blue");
         q.setAnswer("Sort");
         q.setAnswer("Blå");
-
+        
         GridPane pane = new GridPane();
-
+        
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
-
-//        pane.setGridLinesVisible(true);
+        
         pane.setPadding(new Insets(10, 10, 10, 150));
         pane.setVgap(10);
         pane.setHgap(50);
         Image i = new Image("blueandblack.jpg");
         ImageView img = new ImageView();
-//        img.setFitWidth(400);
-//        img.setFitHeight(800);
-//        pane.setPrefWidth(800);
+        
         img.setImage(i);
         img.setFitHeight(500);
         img.setFitWidth(350);
         Label lblQuestion = new Label("Name one of the colors in this dress");
         lblQuestion.setTextFill(Color.GREY);
         lblQuestion.setFont(Font.font("Impact", 24));
-        
-        lblAnswer.setFont(Font.font("Impact", 24));
 
+        lblAnswer.setFont(Font.font("Impact", 24));
+        
         txfAnswer = new TextField();
         Button btnOK = new Button("Answer");
         btnOK.setOnAction(event -> answer());
-        
+
         VBox vbx = new VBox(20, lblQuestion, txfAnswer, btnOK, lblAnswer);
         pane.add(img, 0, 0);
         pane.add(vbx, 1, 0);
         vbx.setAlignment(Pos.CENTER);
-
+        
     }
-    
+
     private void answer() {
         String response = txfAnswer.getText();
         if (q.checkAnswer(response) == true) {
@@ -80,6 +77,6 @@ public class QuestionDemo5 extends Application {
             lblAnswer.setText("False!");
             lblAnswer.setTextFill(Color.RED);
         }
-
+        
     }
 }
