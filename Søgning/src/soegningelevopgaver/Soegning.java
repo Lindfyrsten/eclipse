7d8054fe73f577d7f4e276be7dcbbd245c876d7d
Turfy <kristianlindbjerg@live.dk>
2016-11-30 12:07:27 +0100

    ewe

--- Søgning/src/soegningelevopgaver/Soegning.java
+++ Søgning/src/soegningelevopgaver/Soegning.java
@@ -3,15 +3,15 @@ package soegningelevopgaver;
 import java.util.ArrayList;
 
 public class Soegning {
-
+    
     public boolean findUlige(int[] tabel) {
-
+        
         boolean found = false;
         int i = 0;
         while (!found && i < tabel.length) {
-            
-            if (tabel[i] % 2 != 0) {
 
+            if (tabel[i] % 2 != 0) {
+                
                 found = true;
                 return found;
             }
@@ -20,20 +20,20 @@ public class Soegning {
             }
         }
         return found;
-
+        
     }
-
+    
     // her skriver du metoder tilopgaverne 2,3,5,6 og 7
-
+    
     //opgave 2 - finder første heltal i intervallet [10,15]
-
+    
     public int findHeltalInterval(int[] tabel) {
-
+        
         boolean found = false;
         int i = 0;
         while (!found && i < tabel.length) {
             if (tabel[i] % 2 == 0 && tabel[i] < 15 && tabel[i] > 9) {
-
+                
                 found = true;
                 return tabel[i];
             }
@@ -42,19 +42,19 @@ public class Soegning {
             }
         }
         return -1;
-
+        
     }
-
+    
     //opgave 3 - nabotal ens?
-
+    
     public boolean findNeighbourDuplicate(int[] tabel) {
-
+        
         boolean found = false;
         int i = 0;
         int oldValue = -1;
         while (!found && i < tabel.length) {
             if (tabel[i] == oldValue) {
-
+                
                 found = true;
                 return found;
             }
@@ -64,40 +64,36 @@ public class Soegning {
             }
         }
         return found;
-
+        
     }
-    
+
     //opgave 5 - find kvadratroden lineært og binær
-
+    
     public int findSquareRootLineær(int tal) {
-
+        
         boolean found = false;
-        int result = 0;
         int i = 0;
         while (!found) {
             if (i * i <= tal && tal < (i + 1) * (i + 1)) {
                 found = true;
-                if (found) {
-                    
-                    result = i;
-                }
+                
             }
             i++;
-            
-        }
 
-        return result;
-    }
-    
-    public int findSquareRootBinær(int tal) {
+        }
         
+        return i - 1;
+    }
+
+    public int findSquareRootBinær(int tal) {
+
         boolean found = false;
         int left = 0;
         int right = tal;
         int middle = -1;
         while (!found && left <= right) {
             middle = (left + right) / 2;
-            
+
             if (middle * middle <= tal && tal < (middle + 1) * (middle + 1)) {
                 found = true;
             }
@@ -115,9 +111,9 @@ public class Soegning {
             return -1;
         }
     }
-    
+
     //Opgave 6
-    
+
     public int swapFind(ArrayList<Integer> arr, int tal) {
         boolean found = false;
         int i = 0;
--- Søgning/src/soegningelevopgaver/SoegningApp.java
+++ Søgning/src/soegningelevopgaver/SoegningApp.java
@@ -1,5 +1,7 @@
 package soegningelevopgaver;
 
+import java.util.ArrayList;
+
 public class SoegningApp {
     
     public static void main(String[] args) {
@@ -7,13 +9,13 @@ public class SoegningApp {
         
         // Kode til afprøvning af opgave 1
         
-        int[] talArray = { 2, 4, 8, 2 };
-        int[] talArrayUdvidet = { 7, 56, 34, 3, 7, 14, 13, 4 };
-        int[] talArrayDuplicate = { 7, 8, 8, 10 };
-        System.out.println("Er der et ulige tal i talArray ? " + s.findUlige(talArray));
-        talArray[2] = 15;
-        System.out.println("Er der et ulige tal i talArray ? " + s.findUlige(talArray));
-        
+//        int[] talArray = { 2, 4, 8, 2 };
+//        int[] talArrayUdvidet = { 7, 56, 34, 3, 7, 14, 13, 4 };
+//        int[] talArrayDuplicate = { 7, 8, 8, 10 };
+//        System.out.println("Er der et ulige tal i talArray ? " + s.findUlige(talArray));
+//        talArray[2] = 15;
+//        System.out.println("Er der et ulige tal i talArray ? " + s.findUlige(talArray));
+//
         // Her tilføjes kode til at afprøve opgaverne 2,3,5,6 og 7
         
         //oggave 2
@@ -48,21 +50,23 @@ public class SoegningApp {
 //            + sm.findSpillerHøjdeScore(spillere));
 
         //opgave 5
-//        System.out.println("Heltalskvadratroden af 111 =  " + s.findSquareRootLineær(111));
-//        System.out.println("Heltalskvadratroden af 10 =  " + s.findSquareRootBinær(10));
+        System.out.println("Heltalskvadratroden af 111 =  " + s.findSquareRootLineær(111));
+        System.out.println("Heltalskvadratroden af 10 =  " + s.findSquareRootBinær(10));
 
         //opgave 6
 
-//        System.out.println("********Opgave 6*********");
-//        ArrayList<Integer> arr1 = new ArrayList<>();
-//        arr1.add(6);
-//        arr1.add(4);
-//        arr1.add(8);
-//        arr1.add(13);
-//        arr1.add(2);
-//        System.out.println(arr1);
-//        System.out.println(s.swapFind(arr1, 13));
-//        System.out.println(arr1);
+        System.out.println("********Opgave 6*********");
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        arr1.add(6);
+        arr1.add(4);
+        arr1.add(8);
+        arr1.add(13);
+        arr1.add(2);
+        System.out.println(arr1);
+        System.out.println("Nye index for tallet 13: " + s.swapFind(arr1, 13));
+        System.out.println(arr1);
+        
+        System.out.println(-3 % 2);
     }
     
 }
